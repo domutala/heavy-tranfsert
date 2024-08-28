bf061a0747577babcc39270520f0e32e02636a81
Mamadou Talla DIA <33329431+domutala@users.noreply.github.com>
2024-08-28 21:27:52 +0000

    download file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,20 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-import { writeFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import { v4 as uuidv4 } from 'uuid';
+import { Response } from 'express';
+
+interface IFyle {
+  name: string;
+  type: string;
+  data: string;
+  size: number;
+}
+
+interface ITransfer {
+  file: IFyle;
+}
 
 @Controller()
 export class AppController {
@@ -13,17 +25,36 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Post('/transfert')
-  transfert(@Body() data: { files: any[] }) {
-    for (const file of data.files) {
-      const id = uuidv4();
+  @Post('/transfer')
+  transfer(@Body() body: ITransfer) {
+    const id = uuidv4();
 
-      writeFileSync(
-        join(process.cwd(), '..', '_FILES_', `${id}.json`),
-        JSON.stringify(file),
-      );
-    }
+    writeFileSync(
+      join(process.cwd(), '..', '_FILES_', `${id}.json`),
+      JSON.stringify(body),
+    );
 
-    return data;
+    return body;
+  }
+
+  @Get('/download')
+  download(@Res() res: Response, @Query('id') id: string) {
+    const transfer = JSON.parse(
+      readFileSync(join(process.cwd(), '..', '_FILES_', `${id}.json`), 'utf-8'),
+    ) as ITransfer;
+
+    const file = transfer.file;
+    const buffer = Buffer.from(
+      file.data.replace(/.*(base64,)/gm, ''),
+      'base64',
+    );
+    const mimeType = file.type;
+
+    res.set({
+      'Content-Type': mimeType,
+      'Content-Disposition': `attachment; filename="${file.name}"`,
+    });
+
+    res.send(buffer);
   }
 }
