65fe9fe4fd3931360ca7c76edc1691a79970aa3e
Mamadou Talla DIA <33329431+domutala@users.noreply.github.com>
2024-08-28 01:49:11 +0000

    init upload

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller()
 export class AppController {
@@ -8,5 +11,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/transfert')
+  transfert(@Body() data: any) {
+    const id = uuidv4();
+    writeFileSync(
+      join(process.cwd(), '_FILES_', `${id}.json`),
+      JSON.stringify(data),
+    );
+
+    return data;
   }
 }
